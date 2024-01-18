5cc13f5b93460012ab7539ac2ae42cc0891c800a
kkankkandev <xowl5460@naver.com>
2024-01-18 15:04:26 +0900

    you're doing great

---
+++ 014-function/11-recursion-func.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	fmt.Println(factorial(10))
+	fmt.Println(loopfactorial(10))
+}
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func loopfactorial(num int) int {
+	var sum int = 1
+	for ; num>0; num-- {
+		sum *= num
+	}
+
+	return sum
+}
